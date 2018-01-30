ec0224b73c1f46662415052c6973bfe9d5224253
Nasim Akhtar <nasimakhtar1996@gmail.com>
2018-01-30 17:14:15 +0530

    js setup

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -6,9 +6,17 @@ import Navbar from '../components/Navbar';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Gatsby + Netlify CMS">
+    <link rel="stylesheet" href="assets/css/main.css" />
+    </Helmet>
     <Navbar />
     <div>{children()}</div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.scrollex.min.js"></script>
+    <script src="assets/js/jquery.scrolly.min.js"></script>
+    <script src="assets/js/skel.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
   </div>
 );
 
